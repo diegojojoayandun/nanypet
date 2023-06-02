f3f599b78ac4bdfb60a0c3c97e20dca0260d2e0f
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-06-02 14:50:06 -0500

    UPDATE: fix creddentials validation, which allow generate token for any unregistered email, add async task and bool method

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using NanyPet.Api.Models;
 using NanyPet.Api.Models.Dto.User;
 using NanyPet.Repositories;
 using System.IdentityModel.Tokens.Jwt;
@@ -24,10 +22,10 @@ namespace NanyPet.Api.Controllers
         }
 
         [HttpPost("login")]
-        public IActionResult Login([FromBody] UserDto user)
+        public async Task<IActionResult> Login([FromBody] UserDto user)
         {
             // Validate user credentials
-            if (!IsValidUserCredentials(user.Email, user.Password))
+            if (await IsValidUserCredentials(user.Email, user.Password) == false)
             {
                 return Unauthorized(); // Return 401 Unauthorized if credentials are invalid
             }
@@ -38,16 +36,16 @@ namespace NanyPet.Api.Controllers
             return Ok(new { token });
         }
 
-        private  bool IsValidUserCredentials(string email, string password)
+        private async Task<bool> IsValidUserCredentials(string email, string password)
         {
             // Perform your user authentication logic here
-            // You can check against a database, an external service, or any other method
+            
             if (email == null || password == null)
                 return false;
 
-            var UserEmail = _userRepository.GetUserByEmail(v => v.Email == email);
+            var UserEmail = await  _userRepository.GetUserByEmail(v => v.Email == email);
             //var UserPassword = _userRepository.GetUserById(v => v.Password == password);
-            // Example validation: hard-coded credentials
+            
             if (UserEmail != null)
             {
                 return true;
@@ -58,39 +56,20 @@ namespace NanyPet.Api.Controllers
 
         private string GenerateJwtToken(UserDto user)
         {
-            //    var tokenHandler = new JwtSecurityTokenHandler();
-            //    var key = Encoding.ASCII.GetBytes(_jwtSettings.SecretKey);
-
-            //    var tokenDescriptor = new SecurityTokenDescriptor
-            //    {
-            //        Subject = new ClaimsIdentity(new[]
-            //        {
-            //    new Claim(ClaimTypes.Name, "diego jojoa") // Add any additional claims here
-            //}),
-            //        Expires = DateTime.UtcNow.AddHours(1),
-            //        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-            //        Issuer = _jwtSettings.Issuer,
-            //        Audience = _jwtSettings.Audience
-            //    };
-
-            //    var token = tokenHandler.CreateToken(tokenDescriptor);
-            //    return tokenHandler.WriteToken(token);
-
-
             // Header
 
             var _symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]));
             var signingCredentials = new SigningCredentials(_symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
             var header = new JwtHeader(signingCredentials);
 
-            // claims
+            // Claims
 
             var claims = new List<Claim>
             {
                 new Claim(ClaimTypes.Name, user.Email),
             };
 
-            // payload
+            // Payload
 
             var payload = new JwtPayload
             (
